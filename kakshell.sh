4cf8b6d49d3aa64025850c9991c9c1e2200b1969
mr.root <42474690+kakroot@users.noreply.github.com>
2020-03-09 06:36:56 +0300

    Create kakshell.sh

---
+++ kakshell.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+clear
+
+echo   "########################################"
+echo  "#‌   this tools Great by kakshell         #"
+echo  "#       follow my instagram              #"
+echo  "#        @sh4hm                          #"
+echo  "#                                        #"
+echo  "##########################################"
+sleep 3
+
+
+
+
+echo "       sh4hom  "
+sleep 1
+clear
+echo "kakshell ./ "
+sleep 1
+clear
+echo "kakshell ..\ "
+sleep 1
+clear
+echo "kakshell .../ "
+sleep 1
+clear
+echo "kakshell ....\ "
+sleep 1
+clear
+echo "kakshell ...../ "
+sleep 1
+clear
+echo "kakshell ......\ "
+sleep 1
+clear
+echo "kakshell ......./ "
+sleep 1
+clear
+echo "kakshell ........\ "
+sleep 1
+clear
+echo "kakshell........./ "
+sleep 1
+clear
+echo "kakshell  ..........\ "
+echo "
+"
+
+termux-setup-storage
+clear
+sleep 2
+echo "       bust ta pka dawnload abet" nano 
+pkg intsall nano -y
+sleep 2 
+echo "    nano ba sarkawtuy dawnload bw" 
+clear
+echo "      busta ta python dawnload abet "
+sleep 2
+pkg install python -y 
+sleep 1
+echo "    pythonish ba sarkawtuy dawnload bw" 
+clear
+echo "      busta ta python2 tawaw abet"
+pkg install python3 -y
+sleep 2 
+echo "       python3 tawaw bu"
+clear
+echo "       bust ba python2 dawnload bet" 
+clear
+echo "        busta ruby dawnload bet"
+pkg install ruby -y
+sleep 2
+echo "  rubish tawaw bw "
+clear
+echo      "busta ba openssh dawnload bet "
+pkg install openssl-tool  -y
+sleep 2
+echo "      openssh dawnload bu tawaw" 
+clear
+echo       "busta ta curl dawnload bet" 
+sleep 2
+pkg install curl -y
+sleep 2
+echo  "      curlish dawnload bu tawaw" 
+sleep 1
+clear 
+echo "        bust ba git dawnload bet" 
+pkg install git -y 
+sleep 2 
+echo "        git ba  tawawy dawnload bw" 
+clear
+echo "       busta ta figlet dawnload abet"
+pkg install figlet -y
+sleep 2
+echo "         figtlet ish tawaw bw" 
+clear
+echo "        bust ta unzip tawaw abet"
+pkg install unzip -y
+sleep 2
+echo "       unzip dawnload bw tawaw" 
+clear
+echo "     busta ba zip dawnload bet" 
+pkg install zip -y
+sleep 2 
+echo "       zip  awnload bw" 
+clear
+
+cd $HOME 
+cd ..
+rm -rif usr/etc/motd
+cd 
+cd ..
+cd usr/etc
+mkdir shahom
+cd shahom
+echo slaw bram am termux to la layn sh4hom chakrawa bo shty zyatr follow instakam bkan  @sh4hom >kakshell.txt
+cd $HOME
+cd /sdcard
+mkdir shahowashere
+cd shahowashere 
+echo slaw bram am termux to la layn sh4hom chakrawa bo shty zyatr follow instakam bkan  @sh4hom >kakshell.txt
+cd $HOME
+pkg install sl -y
+sleep 1
+clear
+echo  "   xwat lagal bram tawaw bw followm bkan @Sh4hom insta"
+sleep 1
+clear
+echo  "   xwat lagal bram tawaw bw followm bkan @Sh4hom insta"
+sleep 1
+clear
+echo  "   xwat lagal bram tawaw bw followm bkan @Sh4hom insta"
+sleep 1
+clear
+echo  "   xwat lagal bram tawaw bw followm bkan @Sh4hom insta"
+sleep 1
+clear
+echo  "   xwat lagal bram tawaw bw followm bkan @Sh4hom insta"
+sleep 1
+clear
+
+echo  "   xwat lagal bram tawaw bw followm bkan @Sh4hom insta"
+clear
+sleep 1
+echo  "   xwat lagal bram tawaw bw followm bkan @Sh4hom insta"
