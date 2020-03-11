e4bd6f0bc505885b063d749d60d915bffc90b37b
mr.root <42474690+kakroot@users.noreply.github.com>
2020-03-11 07:48:45 +0300

    Update kakshell.sh

--- kakshell.sh
+++ kakshell.sh
@@ -19,97 +19,166 @@ sleep 3
 echo "       sh4hom  "
 sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;90m          kakshell ./  \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
 sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;96m          kakshell ..\  \e[0m\n  "
+printf "\e[1;92m########################################\e[0m\n"
 sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m          kakshell .../ \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m           kakshell ....\  \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
 sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m           kakshell ...../ \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
 sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m         kakshell ......\ \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
 sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m          kakshell ......./ \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
 sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m         kakshell ........\\e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
 sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m        kakshell........./  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m       kakshell  ..........\ "
+printf "\e[1;92m########################################\e[0m\n"
 echo "
 "
 
 termux-setup-storage
 clear
 sleep 2
-printf "\e[1;91m          bust ta pka dawnload abet nano \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m          bust ta pka dawnload abet nano \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 sleep 2
 pkg intsall nano -y
 sleep 2 
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m          nano ba sarkawtuy dawnload bw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
 sleep 2
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m          busta ta python dawnload abet \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 sleep 2
+
 pkg install python -y 
 sleep 1
-printf "\e[1;91m          pythonish ba sarkawtuy dawnload bw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m          pythonish ba sarkawtuy dawnload bw  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 clear
 
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m           busta ta python2 tawaw abet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 pkg install python3 -y
 sleep 2 
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m                   python3 tawaw bu  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 clear
-printf "\e[1;91m              bust ba python2 dawnload bet  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              bust ba python2 dawnload bet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m               busta ruby dawnload bet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 pkg install ruby -y
 sleep 2
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m                  rubish tawaw bw \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m            busta ba openssh dawnload bet\e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 pkg install openssl-tool  -y
 sleep 2
-printf "\e[1;91m             openssh dawnload bu tawaw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m             openssh dawnload bu tawaw  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 clear
-printf "\e[1;91m              busta ta curl dawnload bet  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              busta ta curl dawnload bet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 sleep 2
 pkg install curl -y
 sleep 2
-printf "\e[1;91m              curlish dawnload bu tawaw  \e[0m\n" 
-sleep 1
 clear 
-printf "\e[1;91m              bust ba git dawnload bet  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              curlish dawnload bu tawaw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+sleep 2
+clear 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              bust ba git dawnload bet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 pkg install git -y 
 sleep 2 
-printf "\e[1;91m              git ba  tawawy dawnload bw  \e[0m\n  " 
 clear
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m              git ba  tawawy dawnload bw  \e[0m\n  "
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
+clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m             busta ta figlet dawnload abet \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 pkg install figlet -y
 sleep 2
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m                figtlet ish tawaw bw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m             bust ta unzip tawaw abet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 pkg install unzip -y
 sleep 2
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m              unzip dawnload bw tawaw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+sleep 1
 clear
-printf "\e[1;91m             busta ba zip dawnload bet  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
+printf "\e[1;91m             busta ba zip dawnload bet  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 pkg install zip -y
-sleep 2 
+clear
+sleep 2
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m                 zip  awnload bw  \e[0m\n" 
+printf "\e[1;92m########################################\e[0m\n"
 clear
 
 cd $HOME 
@@ -130,27 +199,40 @@ cd $HOME
 pkg install sl -y
 sleep 1
 clear
-printf "\e[1;91m       xwat lagal bram tawaw bw followm bkan @Sh4hom insta  \e[0m\n"
+printf "\e[1;92m##############################################################\e[0m\n"
+printf "\e[1;91m       xwat lagal bram tawaw bw followm bkan @Sh4hom insta    \e[0m\n"
+printf "\e[1;92m#############################################################\e[0m\n"
 sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m          xwat lagal bram tawaw bw followm bkan @Sh4hom insta  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m       xwat lagal bram tawaw bw followm bkan @Sh4hom insta  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m        xwat lagal bram tawaw bw followm bkan @Sh4hom insta  \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m        xwat lagal bram tawaw bw followm bkan @Sh4hom insta   \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 sleep 1
 clear
+printf "\e[1;92m########################################\e[0m\n"
 
 printf "\e[1;91m       xwat lagal bram tawaw bw followm bkan @Sh4hom insta \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 clear
 sleep 1
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m       xwat lagal bram tawaw bw followm bkan @Sh4hom insta  \e[0m\n"
-printf "\e[1;92m(         \e[0m\n"
+printf "\e[1;92m########################################\e[0m\n"
 sleep 5
 clear 
 read -s -t 1
@@ -159,7 +241,10 @@ sleep 2
 git clone https://github.com/thelinuxchoice/instashell -l 
 
 sleep 1 
+clear 
+printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m          bust insta shell active akam bot \e[0m\n "
+printf "\e[1;92m########################################\e[0m\n"
 sleep 5
 cd instashell 
 chmod 777 *
@@ -198,3 +283,4 @@ printf "\e[1;94m#                                      # \e[0m\n"
 printf "\e[1;94m######################################## \e[0m\n"
 sleep 3
 figlet good bye 
+printf "\e[1;92m instagram  @sh4hom \e[0m\n"
