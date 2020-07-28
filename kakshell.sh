2d0132875c95ddf3dc4263a5a50e6f0f45015423
sh4hom <42474690+sh4hom@users.noreply.github.com>
2020-07-28 23:29:47 +0300

    Update kakshell.sh

--- kakshell.sh
+++ kakshell.sh
@@ -6,7 +6,7 @@ clear
  cd $HOME
  
 printf "\e[1;92m########################################\e[0m\n"
-printf "\e[1;92m#   this tools Creat by kakshell       # \e[0m\n"           
+printf "\e[1;92m#   this tools creat by kakshell       # \e[0m\n"           
 printf "\e[1;92m#       follow my instagram            # \e[0m\n"
 printf "\e[1;91m#           @sh4hm                     # \e[0m\n"
 printf "\e[1;92m#                                      # \e[0m\n"
@@ -15,9 +15,9 @@ sleep 3
 pkg update -y
 clear
 pkg upgrade -y
+xdg-open https://www.instagram.com/shahokurdsh
 
-
-
+sleep 10
 
 echo "       sh4hom  "
 sleep 1
@@ -196,7 +196,7 @@ cd shahom
 echo slaw bram am termux to la layn sh4hom chakrawa bo shty zyatr follow instakam bkan  @sh4hom >kakshell.txt
 cd $HOME
 cd /sdcard
-mkdir shahowashere
+mkdir sh4hom
 cd shahowashere 
 echo slaw bram am termux to la layn sh4hom chakrawa bo shty zyatr follow instakam bkan  @sh4hom >kakshell.txt
 cd $HOME
@@ -238,47 +238,14 @@ printf "\e[1;92m########################################\e[0m\n"
 printf "\e[1;91m   xwat lagal bram tawaw bw followm bkan @Sh4hom insta  \e[0m\n"
 printf "\e[1;92m########################################\e[0m\n"
 sleep 5
-clear 
-read -s -t 1
-
-sleep 2
-git clone https://github.com/thelinuxchoice/instashell -l 
-
-sleep 1 
-clear 
-printf "\e[1;92m########################################\e[0m\n"
-printf "\e[1;91m bust insta shell active akam bot \e[0m\n "
-printf "\e[1;92m########################################\e[0m\n"
-sleep 5
-cd instashell 
-chmod 777 *
 pkg install tor -y
 cd $HOME
 
+figlet snapchat
+xdg-open https://www.snapchat.com/add/sh4hom
+sleep 10
 
-
-
-
-
- 
 pkg install php -y
- 
- 
-
-
-git clone https://github.com/CiKu370/OSIF -l
-cd OSIF 
-chmod 777 *
-
-pip2 install -r  requirements.txt
-
-
-cd $HOME
-git clone https://github.com/thelinuxchoice/shellphish.git -l
-
-
-
-
 printf "\e[1;94m########################################\e[0m\n"
 printf "\e[1;94m#   this tools Great by kakshell       # \e[0m\n"           
 printf "\e[1;4m#       follow my instagram            # \e[0m\n"
@@ -288,7 +255,4 @@ printf "\e[1;94m######################################## \e[0m\n"
 sleep 3
 figlet good bye 
 printf "\e[1;92m instagram  @sh4hom \e[0m\n"\
-cd $HOME
 
-
-ls
